79760f2c273b78763ca9dc7e81e57dfbaaa58a01
mischakurth <github@mischakurth.de>
2025-12-02 22:40:27 +0100

    2024 day1 created

---
+++ 2024/day01/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    @staticmethod
+    def get_total_distance(file_name):
+        with open(file_name):
+            pass
+
+
+
+
+        total = 0
+
+
+        return total
+
+
+
+s = Solution()
+print('Lösung: ', s.get_total_distance('input.txt'))
