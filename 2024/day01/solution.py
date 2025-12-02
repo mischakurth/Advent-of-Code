c34e06c4ceb88db0509bbaaca356a6b10967b15b
mischakurth <github@mischakurth.de>
2025-12-02 22:47:23 +0100

    Beginn Arbeit

--- 2024/day01/solution.py
+++ 2024/day01/solution.py
@@ -1,8 +1,17 @@
 class Solution:
     @staticmethod
     def get_total_distance(file_name):
-        with open(file_name):
-            pass
+        with open(file_name) as f:
+            whole_list = f.read().split()
+
+        left_list = []
+        right_list = []
+        for i, entry in enumerate(whole_list):
+            if i % 2:
+                right_list.append(entry)
+            else:
+                left_list.append(entry)
+
 
 
 
