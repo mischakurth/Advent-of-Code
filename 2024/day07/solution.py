6939a5ba33b04d613af90e54bc99b14790b604c5
mischakurth <github@mischakurth.de>
2025-12-04 18:03:13 +0100

    Erstellung day07

---
+++ 2024/day07/solution.py
@@ -0,0 +1,12 @@
+file_name = 'input.txt'
+
+def day_07(part):
+    with open(file_name) as f:
+        pass
+    pass
+
+
+
+
+print('Lösung zu Teil 1:', day_07(1))
+print('Lösung zu Teil 2:', day_07(2))
